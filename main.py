1311aa28bf0b1285968cde98f0551a21012ef11e
rudrakshrankawat <rudraksh.23bcs10004@ms.sst.scaler.com>
2025-06-16 17:27:58 +0530

    create subclass PointOnCanvas

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ class Point:
         else:
             return False
         
+class PointOnCanvas(Point):
+    def draw(self, canvas: turtle.Turtle, color="red", size=4):
+        canvas.penup()
+        canvas.goto(self.x, self.y)
+        canvas.pendown()
+        canvas.dot(size, color)
+        
+
+        
 class Rectangle:
     def __init__(self, lowleft: Point, upright: Point):
         self.lowleft = lowleft
@@ -36,7 +45,7 @@ class RectangleOnCanvas(Rectangle):
         canvas.forward(self.upright.x - self.lowleft.x)
         canvas.left(90)
         canvas.forward(self.upright.y - self.lowleft.y)
-        turtle.done()
+        
     
 
 
@@ -46,20 +55,23 @@ if __name__ == "__main__":
                            Point(randint(101,200), randint(101,200)))
     print(f"Rectangle Coordinates: {rectangle.lowleft.x, rectangle.lowleft.y} and {rectangle.upright.x, rectangle.upright.y}")
 
-    rectangle.draw(turtle.Turtle())
-
-
     #ask user to guess a point
     print("Guess a point which lies in the above rectangle:")
-    user_point = Point(float(input("Guess X coordinate:")),
+    user_point = PointOnCanvas(float(input("Guess X coordinate:")),
                     float(input("Guess Y coordinate: ")))
 
+    t = turtle.Turtle()
+    rectangle.draw(canvas=t)
+    user_point.draw(canvas=t)
+    turtle.done()
+
+
     #ask user to guess the area of rectangle
-    user_area = float(input("Guess the area:"))
-    
+    user_area = float(input("Guess the area:"))    
     #print if user point falls in rectangle or not
     print(f"Does the point lie in the rectangle: {"Yes" if user_point.falls_in_reactangle(rectangle) else "No"}")
 
     #print the difference between user's guessed aread vs real area
     print("Your area was off by:", user_area - rectangle.get_area())
+    print("--------SEE YOU AGAIN!--------")
 
--- rectangle.py
+++
@@ -1,9 +0,0 @@
-from point import Point
-class Rectangle:
-    def __init__(self, lowleft: Point, upright: Point):
-        self.lowleft = lowleft
-        self.upright = upright
-
-    def get_area(self) -> float:
-        return (self.upright.x - self.lowleft.x) * \
-                (self.upright.y - self.lowleft.y)
